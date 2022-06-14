4fb93a39185ead7a979e3f60a8ab0bfaa3447b2d
javier ferrales <ferralespereira@gmail.com>
2022-06-14 16:43:26 -0400

    reemplacing jquery for javascript in chat.html

---
+++ app/static/js/chat.js
@@ -0,0 +1,27 @@
+var socket = io.connect('http://' + document.domain + ':' + location.port + '/chat');
+socket.on('connect', function () {
+    socket.emit('joined', {});
+});
+socket.on('status', function (data) {
+    var textare_chat = document.getElementById('chat');
+    textare_chat.value += '<' + data.msg + '>\n';
+    textare_chat.scrollTop = textare_chat.scrollHeight;
+});
+socket.on('message', function (data) {
+    var textare_chat = document.getElementById('chat');
+    textare_chat.value += '<' + data.msg + '>\n';
+    textare_chat.scrollTop = textare_chat.scrollHeight;
+});
+function sendText(e) {
+    var code = e.keyCode || e.which;
+    if (code == 13) {
+        var text = document.getElementById('text');
+        socket.emit('text', { msg: text.value });
+        text.value = '';
+    }
+}
+function leave_room() {
+    socket.emit('left', {}, function () {
+        socket.disconnect();
+    });
+}
---
+++ app/static/js/chat.ts
@@ -0,0 +1,32 @@
+let socket:any = io.connect('http://' + document.domain + ':' + location.port + '/chat');
+
+socket.on('connect', function() {
+    socket.emit('joined', {});
+});
+
+socket.on('status', function(data:any) {
+    let textare_chat:any = document.getElementById('chat');
+    textare_chat.value += '<' + data.msg + '>\n';
+    textare_chat.scrollTop = textare_chat.scrollHeight;
+});
+
+socket.on('message', function(data) {
+    let textare_chat:any = document.getElementById('chat');
+    textare_chat.value += '<' + data.msg + '>\n';
+    textare_chat.scrollTop = textare_chat.scrollHeight;
+});
+
+function sendText(e:any){
+    let code = e.keyCode || e.which;
+    if (code == 13) {
+        let text:any = document.getElementById('text');
+        socket.emit('text', { msg: text.value });
+        text.value ='';
+    }
+}
+
+function leave_room() {
+    socket.emit('left', {}, function () {
+        socket.disconnect();
+    });
+}
