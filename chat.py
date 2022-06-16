add61a1bd7a96b97228c5244d065554148ed69ed
javier ferrales <ferralespereira@gmail.com>
2022-06-16 09:14:26 -0400

    context processor userName()

--- chat.py
+++ chat.py
@@ -1,7 +1,20 @@
 #!/bin/env python
 from app import create_app, socketio
+from flask import session
 
 app = create_app(debug=True)
+
+# context processor
+@app.context_processor
+def userName():
+    if session.get('name'):
+        return{
+                'user_name': session['name']
+            }
+    else:
+        return{
+                'user_name': ''
+            }
 
 if __name__ == '__main__':
     socketio.run(app)
