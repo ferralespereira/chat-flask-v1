81ef65fce25dd83de6b4a88d071c650257efa50b
javier ferrales <ferralespereira@gmail.com>
2022-06-16 17:27:19 -0400

    all in one view

--- app/main/routes.py
+++ app/main/routes.py
@@ -11,14 +11,3 @@ def index():
     session['room'] = form.room.data
 
     return render_template('index.html', form=form)
-
-
-# @main.route('/chat')
-# def chat():
-#     """Chat room. The user's name and room must be stored in
-#     the session."""
-#     name = session.get('name', '')
-#     room = session.get('room', '')
-#     if name == '' or room == '':
-#         return redirect(url_for('.index'))
-#     return render_template('chat.html', name=name, room=room)
