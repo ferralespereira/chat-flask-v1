607f80a30ca0687f3b46fabba4de3e824c61592b
javier ferrales <ferralespereira@gmail.com>
2022-06-17 10:32:37 -0400

    more

--- app/main/forms.py
+++ app/main/forms.py
@@ -5,6 +5,10 @@ from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
     """Accepts a nickname and a room."""
-    name = StringField('Nickname', validators=[DataRequired()])
-    room = StringField('Room', validators=[DataRequired()])
-    submit = SubmitField('Enter Chatroom')
+    # name = StringField('Nickname', validators=[DataRequired()])
+    # room = StringField('Room', validators=[DataRequired()])
+    # submit = SubmitField('Enter Chatroom')
+
+    name = StringField('Username', validators=[DataRequired()])
+    # password = PasswordField('Password', validators=[DataRequired()])
+    # remember_me = BooleanField('Remeber Me')
--- app/main/routes.py
+++ app/main/routes.py
@@ -1,18 +1,18 @@
 from flask import session, redirect, url_for, render_template, request
 from . import main
-# from .forms import LoginForm
+from .forms import LoginForm
 
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
-    """Login form to enter a room."""
-    # form = LoginForm()
-    # session['name'] = form.name.data
-    # session['room'] = form.room.data
-
+    form = LoginForm()
     if request.method == 'POST':
-        session['name'] = request.form['name']
-        session['room'] = request.form['room']
-        return render_template('index.html', name=session['name'])
+        # session['name'] = request.form['name']
+        # session['room'] = request.form['room']
 
-    return render_template('index.html')
+        session['name'] = form.name.data
+        session['room'] = request.form['room']
+
+        return render_template('index.html', form=form)
+
+    return render_template('index.html', form=form)
